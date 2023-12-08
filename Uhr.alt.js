80a2914bef7c954be47b546a8b03dcc63b7be33d
Valentinnin <145351980+Valentinnin@users.noreply.github.com>
2023-12-08 15:47:02 +0100

    professional und retardmode

---
+++ Uhr.alt.js
@@ -0,0 +1,42 @@
+function getTimeInWords() {
+    const now = new Date();
+
+    const stunden = [
+        'zwölfi', 'eis', 'zwei', 'drü', 'vieri', 
+        'füfi', 'sächsi', 'sibni', 'achti', 'nüni', 'zäni', 'elfi'
+    ];
+
+    const minuten = [
+        '', 'eis', 'zwöi', 'drü', 'vier', 
+        'füf', 'sächs', 'sibä', 'acht', 'nün', 'zäh', 
+        'elf', 'zwölf', 'drizäh', 'vierzäh', 'füfzäh',
+        'sächzäh', 'sibzäh', 'achtzäh', 'nünzäh'
+    ];
+
+    let stunde = now.getHours() % 12;
+    let minute = now.getMinutes();
+
+    let output = "Es isch ";
+
+    if (minute == 0) {
+        output += stunden[stunde] + " ";
+    } else if (minute <= 20) {
+        output += minuten[minute] + " ab " + stunden[stunde];
+    } else if (minute < 30) {
+        output += minuten[30 - minute] + " vor halbi " + stunden[(stunde + 1) % 12];
+    } else if (minute == 30) {
+        output += "halbi " + stunden[(stunde + 1) % 12];
+    } else if (minute < 40) {
+        output += minuten[minute - 30] + " ab halbi " + stunden[(stunde + 1) % 12];
+    } else {
+        output += minuten[60 - minute] + " vor " + stunden[(stunde + 1) % 12];
+    }
+
+    document.getElementById("timeInWords").textContent = output;
+}
+
+// Aufruf der Funktion, um die Uhrzeit sofort beim Laden der Seite anzuzeigen
+getTimeInWords();
+
+// Aktualisieren Sie die Uhrzeit jede Minute
+setInterval(getTimeInWords, 60000); // 60000 Millisekunden für eine Minute
--- uhr.js
+++ uhr.js
@@ -1,42 +1,53 @@
 function getTimeInWords() {
-    const now = new Date();
-
-    const stunden = [
-        'zwölfi', 'eis', 'zwei', 'drü', 'vieri', 
+    const hoursInWords = [
+        'zwölfi', 'eis', 'zwöi', 'drü', 'vieri', 
         'füfi', 'sächsi', 'sibni', 'achti', 'nüni', 'zäni', 'elfi'
     ];
 
-    const minuten = [
+    const minutesInWords = [
         '', 'eis', 'zwöi', 'drü', 'vier', 
         'füf', 'sächs', 'sibä', 'acht', 'nün', 'zäh', 
         'elf', 'zwölf', 'drizäh', 'vierzäh', 'füfzäh',
         'sächzäh', 'sibzäh', 'achtzäh', 'nünzäh'
     ];
 
-    let stunde = now.getHours() % 12;
-    let minute = now.getMinutes();
+    const now = new Date();
+    const hour = now.getHours() % 12;
+    const minute = now.getMinutes();
 
-    let output = "Es isch ";
+    const nextHour = (hour + 1) % 12;
 
-    if (minute == 0) {
-        output += stunden[stunde] + " ";
-    } else if (minute <= 20) {
-        output += minuten[minute] + " ab " + stunden[stunde];
-    } else if (minute < 30) {
-        output += "füf vor halbi " + stunden[(stunde + 1) % 12];
-    } else if (minute == 30) {
-        output += "halbi " + stunden[(stunde + 1) % 12];
-    } else if (minute < 40) {
-        output += "füf ab halbi " + stunden[(stunde + 1) % 12];
-    } else {
-        output += minuten[60 - minute] + " vor " + stunden[(stunde + 1) % 12];
+    const timeString = buildTimeString(hour, minute, nextHour, hoursInWords, minutesInWords);
+
+    document.getElementById("timeInWords").textContent = timeString;
+}
+
+function buildTimeString(hour, minute, nextHour, hoursInWords, minutesInWords) {
+    if (minute === 0) {
+        return "Es isch " + hoursInWords[hour];
+    }
+    
+    if (minute <= 20) {
+        return `Es isch ${minutesInWords[minute]} ab ${hoursInWords[hour]}`;
     }
 
-    document.getElementById("timeInWords").textContent = output;
+    if (minute < 30) {
+        return `Es isch ${minutesInWords[30 - minute]} vor halbi ${hoursInWords[nextHour]}`;
+    }
+
+    if (minute === 30) {
+        return `Es isch halbi ${hoursInWords[nextHour]}`;
+    }
+
+    if (minute < 40) {
+        return `Es isch ${minutesInWords[minute - 30]} ab halbi ${hoursInWords[nextHour]}`;
+    }
+
+    return `Es isch ${minutesInWords[60 - minute]} vor ${hoursInWords[nextHour]}`;
 }
 
 // Aufruf der Funktion, um die Uhrzeit sofort beim Laden der Seite anzuzeigen
 getTimeInWords();
 
 // Aktualisieren Sie die Uhrzeit jede Minute
-setInterval(getTimeInWords, 600);
+setInterval(getTimeInWords, 6000); // 6000 Millisekunden für eine Minute
